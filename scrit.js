edd04da44cc3e835bd659d96c2e61b004667fca7
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-23 12:58:01 +0530

    Update scrit.js

--- scrit.js
+++ scrit.js
@@ -9,14 +9,23 @@ const mainBox = document.getElementById("main");
 
 sum = 5
 
+
+let newmark = []
+
 function displaystudentname(){
         ulEl = document.createElement('div');
         mainBox.classList.add('main')
         mainBox.appendChild(ulEl)
         ulEl.classList.add('ulel')
-    ulEl.innerHTML = `<p>name; ${Name.value}</p><samp>avrage 12</samp>`
-    sum = sum -1 ;
+        let testavarage = Number(testMarkEl.value ) +  Number(PreFinalmarkEl.value ) + Number(finalMarkEl.value )
+        total = Math.floor(testavarage / 10);
+        newmark.push[total]
+        ulEl.innerHTML = `<p>name; ${Name.value}</p><samp>avrage ${total}</samp>`
+        sum = sum -1 ;
 }
+console.log(newmark)
+
+
 
 submitEl.addEventListener("click", () => {
     if(sum !== 0){
